401c51df942dbec4fb82c2f9518e030ab28cee21
Fernando Pauer <fpauer@gmail.com>
2016-12-30 13:59:04 -0800

    Hypermedia-Driven RESTful Web Service

---
+++ src/main/java/pauer/spring/rest/HateoasRestApiApplication.java
@@ -0,0 +1,12 @@
+package pauer.spring.rest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HateoasRestApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HateoasRestApiApplication.class, args);
+	}
+}
